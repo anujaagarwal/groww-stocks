bd8ccc29fe9890af8e933629d6bd4df8cd15b325
anujaAgarwal1 <anuja@stockgro.com>
2023-10-29 06:48:54 +0530

    fix:made footer intact in the bottom

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,20 +9,21 @@ require("dotenv").config();
 export default function Home() {
   const [activeTab, setActiveTab] = useState("topGainers"); // Initialized with the default tab
 
-  // // Callback function to update the active tab
+  // Callback function to update the active tab
   const handleTabChange = (tab) => {
     setActiveTab(tab);
   };
+
   return (
-    <div className="bg-white">
-      <div>
-        <Navbar />
+    <div className="flex flex-col min-h-screen bg-white">
+      <Navbar />
+      <div className="flex-grow">
         <div className="mx-auto w-screen">
           <Tabs activeTab={activeTab} onTabChange={handleTabChange} />
           {activeTab === "topGainers" ? <TopGainers /> : <TopLosers />}
-          <Footer />
         </div>
       </div>
+      <Footer />
     </div>
   );
 }
--- src/pages/stock/[id].jsx
+++ src/pages/stock/[id].jsx
@@ -14,6 +14,7 @@ export default function StockInfo() {
   const [stockData, setStockData] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+
   useEffect(() => {
     const fetchData = async (id) => {
       try {
@@ -40,39 +41,41 @@ export default function StockInfo() {
   const hasData = !loading && !error && stockData;
 
   return (
-    <div>
+    <div className="flex flex-col min-h-screen">
       <Navbar />
-      {loading && (
-        <div className="loader">
-          <p>Loading ...</p>
-        </div>
-      )}
-      {error && (
-        <div className="error-message">
-          An error occurred. Please try again later.
-        </div>
-      )}
-      {hasData && (
-        <>
-          <Header
-            symbol={stockData[0].symbol}
-            image={stockData[0].image}
-            exchangeShortName={stockData[0].exchangeShortName}
-            changes={stockData[0].changes}
-            price={stockData[0].price}
-            companyName={stockData[0].companyName}
-          />
-          <CandlestickChart id={id} />
-          <StockDescription
-            symbol={stockData[0].symbol}
-            companyName={stockData[0].companyName}
-            sector={stockData[0].sector}
-            industry={stockData[0].industry}
-            description={stockData[0].description}
-          />
-          <LineChart />
-        </>
-      )}
+      <div className="flex-grow">
+        {loading && (
+          <div className="loader">
+            <p>Loading ...</p>
+          </div>
+        )}
+        {error && (
+          <div className="error-message">
+            An error occurred. Please try again later.
+          </div>
+        )}
+        {hasData && (
+          <>
+            <Header
+              symbol={stockData[0].symbol}
+              image={stockData[0].image}
+              exchangeShortName={stockData[0].exchangeShortName}
+              changes={stockData[0].changes}
+              price={stockData[0].price}
+              companyName={stockData[0].companyName}
+            />
+            <CandlestickChart id={id} />
+            <StockDescription
+              symbol={stockData[0].symbol}
+              companyName={stockData[0].companyName}
+              sector={stockData[0].sector}
+              industry={stockData[0].industry}
+              description={stockData[0].description}
+            />
+            <LineChart />
+          </>
+        )}
+      </div>
       <Footer />
     </div>
   );
